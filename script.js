0830a5c38dd032945aab35bbfd1923f1b8b9af38
Gkmegh <118533859+Gkmegh@users.noreply.github.com>
2023-07-05 15:14:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-//your code here
+let bandNames = ['The Rolling Stones', 'Led Zeppelin', 'Pink Floyd', 'The Beatles', 'Aerosmith'];
 
+function sortBandNames(bandNames) {
+  const articles = ['a', 'an', 'the'];
+  
+  // Remove articles from band names
+  const bandNamesWithoutArticles = bandNames.map(name => {
+    const words = name.split(' ');
+    const filteredWords = words.filter(word => !articles.includes(word.toLowerCase()));
+    return filteredWords.join(' ');
+  });
+  
+  // Sort band names in lexicographic order
+  const sortedBandNames = bandNamesWithoutArticles.sort((a, b) => a.localeCompare(b));
+  
+  // Create HTML list
+  const ul = document.getElementById('band');
+  sortedBandNames.forEach(name => {
+    const li = document.createElement('li');
+    li.textContent = name;
+    ul.appendChild(li);
+  });
+}
+
+// Call the function with the given band names
+sortBandNames(bandNames);
