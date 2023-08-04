bcba0359bdd8ef685fec58036441582254f48ca2
Gkmegh <118533859+Gkmegh@users.noreply.github.com>
2023-08-04 11:37:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
-let bandNames = ['The Rolling Stones', 'Led Zeppelin', 'Pink Floyd', 'The Beatles', 'Aerosmith'];
-
-function sortBandNames(bandNames) {
-  const articles = ['a', 'an', 'the'];
+function sortBands(bands) {
+  const articles = ["a", "an", "the"];
   
-  // Remove articles from band names
-  const bandNamesWithoutArticles = bandNames.map(name => {
-    const words = name.split(' ');
-    const filteredWords = words.filter(word => !articles.includes(word.toLowerCase()));
-    return filteredWords.join(' ');
+  const sortedBands = bands.sort((a, b) => {
+    const nameA = a.toLowerCase();
+    const nameB = b.toLowerCase();
+    
+    // Remove articles from the beginning of the band names
+    for (const article of articles) {
+      if (nameA.startsWith(article + " ")) {
+        nameA = nameA.slice(article.length + 1);
+      }
+      if (nameB.startsWith(article + " ")) {
+        nameB = nameB.slice(article.length + 1);
+      }
+    }
+    
+    return nameA.localeCompare(nameB);
   });
   
-  // Sort band names in lexicographic order
-  const sortedBandNames = bandNamesWithoutArticles.sort((a, b) => a.localeCompare(b));
-  
-  // Create HTML list
-  const ul = document.getElementById('band');
-  sortedBandNames.forEach(name => {
-    const li = document.createElement('li');
-    li.textContent = name;
-    ul.appendChild(li);
-  });
+  return sortedBands;
 }
 
-// Call the function with the given band names
-sortBandNames(bandNames);
+const bandNames = ["The Beatles", "Led Zeppelin", "Pink Floyd", "Aerosmith", "Anathema", "Queen"];
+const sortedBands = sortBands(bandNames);
+
+// Creating an unordered list with sorted band names
+const ul = document.createElement("ul");
+ul.id = "band";
+
+for (const band of sortedBands) {
+  const li = document.createElement("li");
+  li.textContent = band;
+  ul.appendChild(li);
+}
+
+// Add the unordered list to the document
+document.body.appendChild(ul);
+module.exports = sortBands;
